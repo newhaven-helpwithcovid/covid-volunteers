ddf25319bb8fb559f04ddf48369ccbf8b40f1b45
whonore <wolfhonore@gmail.com>
2020-07-29 11:17:08 -0400

    Factor out dev team

---
+++ config/initializers/devs.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+DEVS = {
+  '': [
+    ['Tiffany Chan', 'Team Lead'],
+    ['Chloe Fink', 'Team Deputy'],
+  ],
+  'Web Development Unit': [
+    ['Mark Santolucito', 'Lead Developer'],
+    ['Sarim Abbas', 'Developer'],
+    ['Flaurencya Ciputra', 'Developer'],
+    ['Alex Chen', 'Developer'],
+    ['Joyce Duan', 'Developer'],
+    ['Wolf Honoré', 'Developer'],
+    ['Lance Tan', 'Developer'],
+    ['Jia Wu', 'Lead Visualization Engineer'],
+  ],
+  'Communications Unit': [
+    ['Jill Capotosto', 'Lead Communications Officer'],
+    ['Eda Uzunlar', 'Deputy Communications Officer'],
+  ],
+  'Liaison Unit': [
+    ['Devin Osborne', 'Community Partnership Deputy'],
+  ],
+}
